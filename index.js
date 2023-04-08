5c1e55488fdee9b00420544b63168f607b8f1cf8
KhulupArtemy <artem.khulup@gmail.com>
2023-04-08 18:44:51 +0300

    update

--- index.js
+++ index.js
@@ -59,8 +59,7 @@ app.post('/web-data', async (req, res) => {
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
-                message_text: `Сумма вашей покупки составляет ${totalPrice},
-                ${products.map(item => item.title).join(', ')}`
+                message_text: `Сумма вашей покупки составляет ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
         })
         return res.status(200).json({});
